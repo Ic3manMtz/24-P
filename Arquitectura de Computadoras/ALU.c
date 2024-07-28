c52e2f1c3e2989f04ba6ed86e53f22a2d2517f74
HPLaptop <molap96@gmail.com>
2024-07-28 11:05:26 -0600

    Semana dos|
    Archivos generados en las primeras dos semanas de clases

---
+++ Arquitectura de Computadoras/ALU.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef short int16_t;
+typedef char int8_t;   
+typedef char unit8_t;
+typedef int int32_t;
+
+typedef float float32_t;
+typedef double float64_t;
+
+int main(int argc, char const *argv[]){
+  int32_t i = -1;
+
+  printf("i: %X\n",i);
+  //for(i=0; i<148; i++)
+  //  printf("i: %d\n", i);
+
+  return 0;
+}
